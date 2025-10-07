bce4aa15cdb23bde07ef7c7750bbbcc126b7198e
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-10-07 10:14:44 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,7 +12,14 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("andrew id")) {
-    return "sreed2 <3";
+    return "sreed2";
+  }
+
+  if (query.toLowerCase().includes("plus")) {
+    const nums = query.split(" plus ");
+    const num1 = nums[0].slice(8);
+    const num2 = nums[1].slice(0, nums[1].indexOf(" "));
+    return (parseInt(num1) + parseInt(num2)).toString();
   }
 
   return "";
