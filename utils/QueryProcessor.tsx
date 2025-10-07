3f47fbf534fc18488d4cf6f855c40f110294def0
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-10-07 10:24:04 -0400

    Update QueryProcessor.tsx

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -17,7 +17,7 @@ export default function QueryProcessor(query: string): string {
 
   if (query.toLowerCase().includes("plus")) {
     const nums = query.split(" plus ");
-    const num1 = nums[0].slice(8);
+    const num1 = nums[0].slice(-2);
     const num2 = nums[1].slice(0, nums[1].indexOf(" "));
     return (parseInt(num1) + parseInt(num2)).toString();
   }
